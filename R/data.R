0ea96a29f67aa5de113a2242fd97b34bf9d06b5b
rstrotmann <rstrotmann@users.noreply.github.com>
2026-06-16 09:27:04 +0200

    update documentation

--- R/data.R
+++ R/data.R
@@ -263,3 +263,23 @@
 #' @source Fully synthetic data set to demonstrate CYP mRNA induction data
 #' evaluation.
 "examplinib_in_vitro_ind"
+
+
+#' in vitro sample data for CYP3 mRNA induction
+#'
+#' @description
+#' CYP3A4 mRNA fold-change data at different precipitant concentrations
+#'
+#' @format
+#' A data frame with 7 columns
+#' * DONOR Hepatocyte donor
+#' * TYPE Test  or positive control
+#' * CONC Precipitant concentration
+#' * OBJECT DDI target
+#' * FOLD mRNA fold change
+#' * REL mRNA induction in percent relative to positive control
+#' * SOURCE Source study report
+#'
+#' @source Fully synthetic data set to demonstrate CYP mRNA induction data
+#' evaluation.
+"examplinib_in_vitro_ind1"
